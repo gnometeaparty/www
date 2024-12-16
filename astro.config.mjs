000d4569a8fa53d677ac32414a4856fa50487898
Elijah Passmore <elijah.passmore@gnometeaparty.com>
2024-12-16 19:25:32 +1300

    feat: add basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import parytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gnometeaparty.com",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+  integrations: [parytown(), sitemap(), tailwind()],
+  experimental: {
+    responsiveImages: true,
+    svg: {
+      mode: "inline",
+    },
+  },
 });
