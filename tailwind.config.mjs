38fd2bf01d30e6abfbc502029386092c60947826
Elijah Passmore <elijah.passmore@gnometeaparty.com>
2024-12-25 07:35:05 +1300

    feat: add basic layout (#1)
    * feat: add basic layout
    
    * build: add fathom id

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: "hsl(var(--primary))",
+        accent: "hsl(var(--accent))",
+      },
+    },
+  },
+  plugins: [typography],
+};
